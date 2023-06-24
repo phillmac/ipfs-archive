24e3b0fc6ed4e73c6b53e962b7c7c4b014b7db9a
phillmac <phillmackintosh@gmail.com>
2023-06-24 07:26:05 +0000

    standardize error output

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -307,7 +307,8 @@ func (s *Scraper) fetch(resource Resource) {
 
 	transformedResp, err := resource.Transform(s, realUrl, respReader)
 	if err != nil {
-		s.Log.Error(fmt.Sprintf("Error transforming resource: %s", realUrl), zap.Error(err))
+		s.Log.Error(fmt.Sprintf("Error transforming resource", realUrl), zap.Error(err), zap.String("url", realUrl.String()),
+	)
 		return
 	}
 
